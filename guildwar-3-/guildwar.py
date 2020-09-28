d63591c2c211936692d440c1a6721cbb99ec892e
BUMJH0121 <40021475+BUMJH0121@users.noreply.github.com>
2020-09-28 22:19:40 +0900

    Add files via upload
    5일차

--- guildwar-3-/guildwar.py
+++ guildwar-3-/guildwar.py
@@ -51,44 +51,46 @@ class I_Dialog(QDialog, input.Ui_i_dialog):
 
 
     def push(self):
-        db = pymysql.connect(host ='localhost', port=3306, user = "root", passwd='112200', db='mydb', charset='utf8')
-        cursor = db.cursor(pymysql.cursors.DictCursor)
         defense = [self.D_LEADER.currentText(), self.D_SECOND.currentText(), self.D_THIRD.currentText()]
         attack = [self.A_LEADER.currentText(), self.A_SECOND.currentText(), self.A_THIRD.currentText()]
-        wod = self.WOD.currentText()
-        temp = "SELECT * FROM attack_member where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
-        cursor.execute(temp, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2]))
-        result = cursor.fetchall()
-        cursor.execute(temp, (defense[0], defense[2], defense[1], attack[0], attack[1], attack[2]))
-        result += cursor.fetchall()
-        cursor.execute(temp, (defense[0], defense[1], defense[2], attack[0], attack[2], attack[1]))
-        result += cursor.fetchall()
-        cursor.execute(temp, (defense[0], defense[2], defense[1], attack[0], attack[2], attack[1]))
-        result += cursor.fetchall()
-
-        if result == ():
-            if wod == '승':
-                p = "INSERT INTO attack_member(d_leader, d_second, d_third, a_leader, a_second, a_third, w, f) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                cursor.execute(p, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2], 1, 0))
+        if self.D_LEADER.findText(defense[0]) != -1 and self.D_SECOND.findText(defense[1]) != -1 and self.D_THIRD.findText(defense[2]) != -1 and self.A_LEADER.findText(attack[0]) != -1 and self.A_SECOND.findText(attack[1]) != -1 and self.A_THIRD.findText(attack[2]) != -1:
+            db = pymysql.connect(host ='localhost', port=3306, user = "root", passwd='112200', db='mydb', charset='utf8')
+            cursor = db.cursor()
+            defense = [self.D_LEADER.currentText(), self.D_SECOND.currentText(), self.D_THIRD.currentText()]
+            attack = [self.A_LEADER.currentText(), self.A_SECOND.currentText(), self.A_THIRD.currentText()]
+            wod = self.WOD.currentText()
+            temp = "SELECT * FROM attack_member where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
+            cursor.execute(temp, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2]))
+            result = cursor.fetchall()
+            cursor.execute(temp, (defense[0], defense[2], defense[1], attack[0], attack[1], attack[2]))
+            result += tuple(cursor.fetchall())
+            cursor.execute(temp, (defense[0], defense[1], defense[2], attack[0], attack[2], attack[1]))
+            result += tuple(cursor.fetchall())
+            cursor.execute(temp, (defense[0], defense[2], defense[1], attack[0], attack[2], attack[1]))
+            result += tuple(cursor.fetchall())
+            if result == ():
+                if wod == '승':
+                    p = "INSERT INTO attack_member(d_leader, d_second, d_third, a_leader, a_second, a_third, w, f) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+                    cursor.execute(p, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2], 1, 0))
+                else:
+                    p = "INSERT INTO attack_member(d_leader, d_second, d_third, a_leader, a_second, a_third, w, f) VALUES (%s, %s, %s, %s, %s, %s, %s, %S)"
+                    cursor.execute(p, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2], 0, 1))
             else:
-                p = "INSERT INTO attack_member(d_leader, d_second, d_third, a_leader, a_second, a_third, w, f) VALUES (%s, %s, %s, %s, %s, %s, %s, %S)"
-                cursor.execute(p, (defense[0], defense[1], defense[2], attack[0], attack[1], attack[2], 0, 1))
-        else:
-            if wod == '승':
-                p = "UPDATE attack_member set w = %s where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
-                cursor.execute(p, (result['w']+1,result['d_leader'], result['d_second'], result['d_third'], result['a_leader'], result['a_second'], result['a_third']))
-            else:
-                p = "UPDATE attack_member set f = %s where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
-                cursor.execute(p, (result['f']+1,result['d_leader'], result['d_second'], result['d_third'], result['a_leader'], result['a_second'], result['a_third']))
-        db.commit()
-        db.close()
+                if wod == '승':
+                    p = "UPDATE attack_member set w = %s where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
+                    cursor.execute(p, (result[0][6]+1,result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5]))
+                else:
+                    p = "UPDATE attack_member set f = %s where d_leader = %s and d_second = %s and d_third = %s and a_leader = %s and a_second = %s and a_third = %s"
+                    cursor.execute(p, (result[0][7]+1,result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], result[0][5]))
+            db.commit()
+            db.close()
+        else :
+            QMessageBox.about(self, 'ERROR', '잘못된 몬스터 이름입니다!')
 
     def combo(self, state, combo):
-        def keyPressEvent(self, e):
-            if e.key() in [Qt.key_Return, Qt.Key_Enter]: 
-                idx = combo.findText(combo.currentText())
-                if idx != -1:
-                    combo.setEditText(combo.itemText(idx))
+        idx = combo.findText(combo.currentText())
+        if idx != -1:
+            combo.setEditText(combo.itemText(idx))
 
 class S_Dialog(QDialog, search.Ui_s_dialog):
     def __init__(self):
@@ -106,26 +108,27 @@ class S_Dialog(QDialog, search.Ui_s_dialog):
         self.close()
 
     def search(self):
-        self.DISPLAY.clear()
-        self.DISPLAY.append('   ' + '리더' + '        ' + '두번째' + '       ' + '세번째' + '       ' + '승률')
-        db = pymysql.connect(host ='localhost', port=3306, user = "root", passwd='112200', db='mydb', charset='utf8')
-        cursor = db.cursor(pymysql.cursors.DictCursor)
         defense = [self.D_LEADER.currentText(), self.D_SECOND.currentText(), self.D_THIRD.currentText()]
-        temp = "SELECT * FROM attack_member where d_leader = %s and d_second = %s and d_third = %s"
-        cursor.execute(temp, (defense[0], defense[1], defense[2]))
-        result = cursor.fetchall()
-        cursor.execute(temp, (defense[0], defense[2], defense[1]))
-        result += tuple(cursor.fetchall())
-        for r in result:
-            self.DISPLAY.append('   ' + r['a_leader'] + '   ' + r['a_second'] + '   ' + r['a_third'] + '    ' + str((r['w']/(r['w']+r['f']))*100))
-        db.close()
+        if self.D_LEADER.findText(defense[0]) != -1 and self.D_SECOND.findText(defense[1]) != -1 and self.D_THIRD.findText(defense[2]) != -1 :
+            self.DISPLAY.clear()
+            self.DISPLAY.append('   ' + '리더' + '        ' + '두번째' + '       ' + '세번째' + '       ' + '승률')
+            db = pymysql.connect(host ='localhost', port=3306, user = "root", passwd='112200', db='mydb', charset='utf8')
+            cursor = db.cursor()
+            temp = "SELECT * FROM attack_member where d_leader = %s and d_second = %s and d_third = %s"
+            cursor.execute(temp, (defense[0], defense[1], defense[2]))
+            result = cursor.fetchall()
+            cursor.execute(temp, (defense[0], defense[2], defense[1]))
+            result += tuple(cursor.fetchall())
+            for r in result:
+                self.DISPLAY.append('   ' + r[3] + '   ' + r[4] + '   ' + r[5] + '    ' + str((r[6]/(r[6]+r[7]))*100))
+            db.close()
+        else:
+            QMessageBox.about(self, 'ERROR', '잘못된 몬스터 이름입니다!')
 
     def combo(self, state, combo):
-        def keyPressEvent(self, e):
-            if e.key() in [Qt.key_Return, Qt.Key_Enter]: 
-                idx = combo.findText(combo.currentText())
-                if idx != -1:
-                    combo.setEditText(combo.itemText(idx))
+        idx = combo.findText(combo.currentText())
+        if idx != -1:
+            combo.setEditText(combo.itemText(idx))
 
 
 if __name__ == '__main__':
